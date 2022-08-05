b89749e56c8bcd94b24b6170397e572d75d63a4b
Bharathmogaveer <bharathmogaveer01@gmail.com>
2022-08-05 12:35:55 +0530

    add google account signin

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,50 +1,69 @@
 import React, { useState } from "react";
 import styles from "../../styles/Login.module.scss";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import { auth } from "../firebase";
-// import { useNavigate } from "react-router-dom";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+import { async } from "@firebase/util";
 
 function Login() {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // const navigate = useNavigate();
-
   const handleLogin = (e) => {
     e.preventDefault();
-
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
         const user = userCredential.user;
-        // navigate("/")
+        console.log(user);
       })
       .catch((error) => {
-        console.log(error);
         setError(true);
       });
+  };
+
+  const provider = new GoogleAuthProvider();
+  const signIn = async () => {
+    const {user}= await signInWithPopup(auth, provider);
+    const {refreshToken, providerData } = user;
+    
+    localStorage.setItem("user", JSON.stringify(providerData));
+    localStorage.setItem("accessToken", JSON.stringify(refreshToken));
   };
 
   return (
     <div className={styles.login_container}>
       <div className={styles.login}>
         <div className={styles.login_header}>Log in to Trello</div>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Enter password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log in</button>
-        {error && <span className="span">wrong email or password </span>}
-      </form>
-    </div>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="Submit">Log in</button>
+          {error && <span className="span">wrong email or password </span>}
+        </form>
+        <Link href={"/auth/register"}>
+          <div className={styles.signup_text}>Sign up for an account</div>
+        </Link>
+        <div className={styles.or}>OR</div>
+        <div className={styles.google} onClick={signIn}>
+          <div></div>
+          <FcGoogle />
+          <div>Sign in with Google</div>
+        </div>
+      </div>
     </div>
   );
 }
